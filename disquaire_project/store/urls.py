ff4cb96fe2096038714e75c32612b95885e5e36c
nassafou <ntyoussouf@gmail.com>
2020-01-03 14:29:01 -0800

    show

---
+++ disquaire_project/store/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+from django.db import models
+
+urlpatterns = [
+    #url(r'^$', views.index ),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^$', views.listing),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail),
+]

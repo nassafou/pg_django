0c6cab066243ce519307be4dfe366475d3937d8e
nassafou <ntyoussouf@gmail.com>
2020-01-20 14:26:12 -0800

    view

--- disquaire_project/core/views.py
+++ disquaire_project/core/views.py
@@ -5,13 +5,13 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-class TestView(APIView):
-    def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'john',
-            'age': 23
-        }
-        return Response(data)
+#class TestView(APIView):
+#    def get(self, request, *args, **kwargs):
+#        data = {
+#            'name': 'john',
+#            'age': 23
+#        }
+#        return Response(data)
 
 # Create your views here.
 #def test_view(request):
